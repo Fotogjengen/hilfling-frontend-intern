6b45e1647465b116e3e224fd8ce793d71509554d
Pernille Klevstuen <peklevstuen@gmail.com>
2019-09-25 22:35:51 +0200

    :bug: Fixed bug so that there is no merge-conflicts on the server after pull

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,8 @@
 
 ssh -i ./deploy_key hilfling@$SERVER_IP_ADDRESS '
 cd /var/www/hilfling-frontend-intern &&
-git pull &&
+git fetch --all && 
+git reset --hard origin/master && 
+git pull origin master &&
 docker-compose up -d --build
 '
